8fa93bb81678c54cdeb0fc3c0d5d32abd15d783f
Matteo Frana <f2@f2.net>
2020-03-13 16:36:19 +0100

    Updated react-bricks 1.0.43; added plugin mainfest and offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby starter React Bricks + Tailwind`,
+        short_name: `React Bricks Starter`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#F65A8E`,
+        display: `minimal-ui`,
+        icon: `src/images/reactbricks-icon.svg`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
       resolve: "gatsby-plugin-postcss",
       options: {
         postCssPlugins: [
