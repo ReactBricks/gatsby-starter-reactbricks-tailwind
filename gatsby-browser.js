1ad52698732e07d540889a91ad9b3e8fb9aedfcd
Matteo Frana <f2@f2.net>
2021-09-03 15:28:00 +0200

    Dark color mode support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactBricks } from 'react-bricks'
 
 import config from './src/react-bricks/config'
 import './src/css/style.css'
 
+const ReactBricksApp = ({ element }) => {
+  const savedColorMode =
+    typeof window === 'undefined' ? '' : localStorage.getItem('color-mode')
+  const [colorMode, setColorMode] = useState(savedColorMode || 'light')
+  const toggleColorMode = () => {
+    const newColorMode = colorMode === 'light' ? 'dark' : 'light'
+    setColorMode(newColorMode)
+    localStorage.setItem('color-mode', newColorMode)
+  }
+
+  const reactBricksConfig = {
+    ...config,
+    isDarkColorMode: colorMode === 'dark',
+    toggleColorMode,
+    contentClassName: `antialiased font-content ${colorMode} ${
+      colorMode === 'dark' ? 'bg-gray-900' : 'bg-white'
+    }`,
+  }
+  return <ReactBricks {...reactBricksConfig}>{element}</ReactBricks>
+}
+
 // Wraps every page in ReactBricks component
 export const wrapPageElement = ({ element }) => {
-  return <ReactBricks {...config}>{element}</ReactBricks>
+  return <ReactBricksApp element={element} />
 }
