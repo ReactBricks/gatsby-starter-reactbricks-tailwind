bce306875163ac6704337eeb963aaaa308998da4
Giorgio Tognoli <giorgio@f2.net>
2022-02-25 18:41:27 +0100

    Updated packages (react-bricks v.3.2.12)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { ReactBricks } from 'react-bricks/frontend'
 
 import config from './src/react-bricks/config'
-import './src/css/tailwind.css'
+import './src/css/style.css'
 
 const ReactBricksApp = ({ element }) => {
   const savedColorMode =
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ReactBricks } from 'react-bricks/frontend'
 
 import config from './src/react-bricks/config'
-import './src/css/tailwind.css'
+import './src/css/style.css'
 
 // Wraps every page in ReactBricks component
 export const wrapPageElement = ({ element }) => {
